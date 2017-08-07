e5dbfbe4d770a3eb8086fb8fb433b015ccc4c18b
Paulrocker <paulofrontend@gmail.com>
2017-08-07 15:24:22 -0300

    arrow using map and reduce

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,7 +1,32 @@
-const country = ['japan', 'island', 'ireland', 'german']
+const allDevices = [{
+  device: 'airConditioning',
+  potency: 1500,
+  hoursForDay: 8,
+  mouth: 30
+}, {
+  device: 'eletricShower',
+  potency: 3300,
+  hoursForDay: 1,
+  mouth: 30
+}, {
+  device: 'freezer',
+  potency: 200,
+  hoursForDay: 10,
+  mouth: 30
+}, {
+  device: 'refrigerator',
+  potency: 350,
+  hoursForDay: 10,
+  mouth: 30
+}, {
+  device: 'lamp',
+  potency: 100,
+  hoursForDay: 6,
+  mouth: 30
+}]
 
+const results = allDevices
+  .map(devices => devices.potency * devices.hoursForDay * devices.mouth)
+  .reduce((acc, item) => acc + item * 0.40)
 
-const likeCountry = country
-  .filter(name => name === 'island')
-  .map(name => `i like ${name}!`).join('')
-console.log(likeCountry)
+console.log(results);
