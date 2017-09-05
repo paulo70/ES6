10ccf3caf78be4cec68ef3c65c393c5042f69f48
Paulrocker <paulofrontend@gmail.com>
2017-09-05 19:03:18 -0300

    using desctructuring for take values of object

---
+++ destructuring-object.js
@@ -0,0 +1,13 @@
+(function() {
+  const person = {
+    name: 'karen campbel',
+    sex: 'male',
+    age: 40,
+    city: 'new york',
+    profission: 'skate'
+  }
+
+  const {name, sex, age} = person
+
+  console.log(`${name} ${sex} ${age}`)
+})()
